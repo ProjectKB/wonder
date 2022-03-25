7daa7d26267b9199ac8035394e7639212f1422b0
Loïc Bertin <bertin.loic01@gmail.com>
2022-03-25 12:31:43 +0100

    detect click outside/inside searchbar component

--- assets/scripts/components/HeaderSearchBar.tsx
+++ assets/scripts/components/HeaderSearchBar.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef, Ref} from 'react'
 import ReactDOM from 'react-dom'
 import axios from '../../../node_modules/axios/index';
 
@@ -11,7 +11,9 @@ const HeaderSearchbar = () => {
   const BASE_URL = window.location.origin
   const [searchValue, setSearchValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-
+  const [showResults, setShowResults] = useState(true);
+  const wrapperRef = useRef(null);
+  
   const FetchQuestionByTitle = async () => {
     try {
       return await axios.post(
@@ -24,7 +26,7 @@ const HeaderSearchbar = () => {
   };
 
   const SearchBarResults = () => (
-    searchResults.length ? <div className='block header-search-result mt-40'>
+    searchResults.length && showResults ? <div className='block header-search-result mt-40'>
         <ul>
           {searchResults.map((searchResult: SearchResult) => (
             <a href={`${BASE_URL}/question/${searchResult.id}`} key={searchResult.id}>
@@ -35,12 +37,35 @@ const HeaderSearchbar = () => {
     </div> : null
   );
 
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      // Alert if clicked on outside of element
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowResults(false);
+        } else {
+          setShowResults(true);
+        }
+      }
+
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  useOutsideAlerter(wrapperRef);
+
   useEffect(() => {
       FetchQuestionByTitle().then(res => setSearchResults(res ? res.data : []));
   }, [searchValue]);
 
   return (
-    <div className="d-flex flex-fill">
+    <div ref={wrapperRef} className="d-flex flex-fill">
       <input className='flex-fill' type="text" value={searchValue} onChange={value => setSearchValue(value.target.value)}></input>
       <SearchBarResults />
     </div>
